568035f0d4fd1521fa29486970b79ce161c584b9
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-05-15 14:38:48 +0200

    fix(tests) : Removed multiple configurations files :bug:

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
-  },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-};
